26387b5813834cf0839a8601ba69d8253177b595
blunk21 <g.bereczki21@gmail.com>
2024-04-17 19:56:07 +0200

    process message

--- iris_poetry/irrigation_manager/irrigation.py
+++ iris_poetry/irrigation_manager/irrigation.py
@@ -2,6 +2,7 @@ from time import sleep
 from multiprocessing import Queue
 from sensor_data.eye_sensor import BleBeaconData
 import multiprocessing as mp
+from process_message import ProcessMessage
 
 logger = mp.get_logger()
 logger.name = "Irrigation"
@@ -10,6 +11,9 @@ def irrigation_process(mes_queue: Queue):
     while True:
         if not mes_queue.empty():
             print("received ble data")
-            beacon_data:BleBeaconData = mes_queue.get()
-            print(beacon_data.__dict__)
+            measurement: ProcessMessage= mes_queue.get()
+            print("Received new message in queue")
+            print(f"Type: {measurement.type}")
+            print(f"Source: {measurement.source}")
+            print(f"Payload: {measurement.payload}")
         sleep(1)
