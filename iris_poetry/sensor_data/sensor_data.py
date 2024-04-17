81e46fc952b9fe4745204a64de8de74b08ad525a
blunk21 <g.bereczki21@gmail.com>
2024-04-17 19:34:53 +0200

    send process message

--- iris_poetry/sensor_data/sensor_data.py
+++ iris_poetry/sensor_data/sensor_data.py
@@ -2,6 +2,7 @@ from time import sleep
 from .eye_sensor import EyeSensor, BleBeaconData
 from multiprocessing import Queue
 import multiprocessing as mp
+from process_message import ProcessMessage
 
 logger = mp.get_logger()
 logger.name = "SensorData"
@@ -14,8 +15,12 @@ def sensor_data_process(
 ):
     sensor = EyeSensor(address="7c:d9:f4:1a:0e:6a")
     while True:
-        mes: BleBeaconData = sensor.get_measurement()
-        measurement_queue.put(mes)
+        measurement: BleBeaconData = sensor.get_measurement()
+        new_message = ProcessMessage()
+        new_message.type = "ble_sensor_measurement"
+        new_message.source = sensor_data_process.__name__
+        new_message.payload = measurement.__dict__
+        measurement_queue.put(new_message)
 
         if not freq_queue.empty():
             new_freq: int = freq_queue.get()
