103bb5935047cfad505faafa995eb916f5703292
blunk21 <g.bereczki21@gmail.com>
2024-04-27 01:11:22 +0200

    added logger to process

--- iris_poetry/system_config/system_config.py
+++ iris_poetry/system_config/system_config.py
@@ -3,10 +3,11 @@ import time
 import os
 import requests
 from dotenv import load_dotenv
+import logging
 
 current_dir = os.path.dirname(__file__)
 parent_dir = os.path.dirname(current_dir)
-env_filepath = os.path.join(parent_dir,".env")
+env_filepath = os.path.join(parent_dir, ".env")
 
 print(env_filepath)
 
@@ -14,7 +15,8 @@ load_dotenv(env_filepath)
 
 
 class SystemConfig:
-    def __init__(self):
+    def __init__(self, logger: logging.Logger):
+        self.logger = logger
         self.alarm1: list[int]
         self.alarm2: list[int]
         self.duration1: int
@@ -34,31 +36,56 @@ class SystemConfig:
             fpath = os.path.join(curdir, "default_config.json")
             with open(fpath, "r") as fo:
                 contents = fo.read()
+                self.logger.info("Loading default config.")
             with open(os.path.join(curdir, "config.json"), "w") as fo:
                 fo.write(contents)
         json_cfg = json.loads(contents)
         self.last_updated = self._load_config(json_cfg)
 
-    def _load_config(self, cfg: dict) -> int:
-        self.alarm1 = cfg["alrm1"]
-        self.alarm2 = cfg["alrm2"]
-        self.duration1 = cfg["dur1"]
-        self.duration2 = cfg["dur2"]
-        self.measurement_frequency = cfg["mes_freq"]
-        self.config_id = cfg["id"]
+    def _save_configuration(self, cfg: dict):
+        content = json.dumps(cfg)
+        curdir = os.path.dirname(__file__)
+        fpath = os.path.join(curdir, "config.json")
 
+        with open(fpath, "w") as fo:
+            fo.write(content)
+        return
+
+    def _load_config(self, cfg: dict) -> int:
+        try:
+            self.alarm1 = cfg["alrm1"]
+            self.alarm2 = cfg["alrm2"]
+            self.duration1 = cfg["dur1"]
+            self.duration2 = cfg["dur2"]
+            self.measurement_frequency = cfg["mes_freq"]
+            self.config_id = cfg["id"]
+        except KeyError:
+            self.logger.error("Failed to read config, unknown key.")
+            return 0
+        self._save_configuration(cfg)
         return round(time.time())
 
-    def update_config(self):
+    def update_config(self) -> bool:
         environment = os.getenv("IRIS_ENVIRONMENT")
-        headers = {"Content-Type":"application/json"}
-        url = "http://" + os.getenv("IRIS_URL_"+environment) + "/configs/" + str(self.config_id) + "/"
-        
-        response = requests.request("GET",url=url,headers=headers)
+        headers = {"Content-Type": "application/json"}
+        url = (
+            "http://"
+            + os.getenv("IRIS_URL_" + environment)
+            + "/configs/"
+            + str(self.config_id)
+            + "/"
+        )
+
+        response = requests.request("GET", url=url, headers=headers)
         if response.status_code != 200:
-            print("Could not get configuration")
-            return
+            self.logger.error("Unable to fetch configuration")
+            self.logger.info(f"Status code:\t{response.status_code}")
+            self.logger.info(f"Content:\t{response.text}")
+            return False
         content = response.text
-        if content:
-            self._load_config(json.loads(content))
-        
+        if content and self._load_config(json.loads(content)):
+            self.logger.info("Successfully loaded configuration")
+            return True
+        else:
+            self.logger.error("Unable to load new configuration")
+            return False
