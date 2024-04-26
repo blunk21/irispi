46dcbf5b8671b4866a2bcc6f1ca0263a798235da
blunk21 <g.bereczki21@gmail.com>
2024-04-27 01:11:16 +0200

    test new config update

--- iris_poetry/system_config/test_sysconfig.py
+++ iris_poetry/system_config/test_sysconfig.py
@@ -11,18 +11,13 @@ def test_default_cfg_loading():
     os.remove(custom_path)
     
     
-def test_config_update():
+def test_config_update_new_config():
     curdir = os.path.dirname(__file__)
-    custom_path = os.path.join(curdir, "config.json")
-    if os.path.exists(custom_path):
-        os.remove(custom_path)
+    config_path = os.path.join(curdir, "config.json")
+    if os.path.exists(config_path):
+        os.remove(config_path)
     config = SystemConfig()
-    print(config.config_id)
-    print("=====Initial config=====\n")
-    print(config.__dict__)
     assert config.config_id == 0
-    
     config.update_config()
     assert config.config_id != 0
-    print(config.__dict__)
-    os.remove(custom_path)
+    os.remove(config_path)
