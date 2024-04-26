1991253a06d0639a16a6e3a7847f7dcc34d9d857
blunk21 <g.bereczki21@gmail.com>
2024-04-26 21:42:59 +0200

    test config update API

--- iris_poetry/system_config/test_sysconfig.py
+++ iris_poetry/system_config/test_sysconfig.py
@@ -1,8 +1,30 @@
 from .system_config import SystemConfig
+import os
+import pickle
 
 
 def test_default_cfg_loading():
+    curdir = os.path.dirname(__file__)
+    custom_path = os.path.join(curdir, "config.json")
     config = SystemConfig()
-    config._load_default_config()
     assert config.alarm1 == [6, 15]
     assert config.duration1 == 10
+    assert os.path.exists(custom_path)
+    os.remove(custom_path)
+    
+    
+def test_config_update():
+    curdir = os.path.dirname(__file__)
+    custom_path = os.path.join(curdir, "config.json")
+    if os.path.exists(custom_path):
+        os.remove(custom_path)
+    config = SystemConfig()
+    print(config.config_id)
+    print("=====Initial config=====\n")
+    print(config.__dict__)
+    assert config.config_id == 0
+    
+    config.update_config()
+    assert config.config_id != 0
+    print(config.__dict__)
+    os.remove(custom_path)
