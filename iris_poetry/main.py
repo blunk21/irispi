c1b900a632240e987c41c6288f29fd1fd96bc629
blunk21 <g.bereczki21@gmail.com>
2024-04-27 01:10:08 +0200

    added config process

--- iris_poetry/main.py
+++ iris_poetry/main.py
@@ -1,22 +1,27 @@
 import multiprocessing as mp
 from multiprocessing import Process, Manager
-from sensor_data.sensor_data import sensor_data_process
-from irrigation_manager.irrigation import irrigation_process
+from sensor_data.sensor_data import process_sensor_data
+from irrigation_manager.irrigation import process_irrigation
+from system_config.config_manager import process_configuration_manager
 
 
 if __name__ == "__main__":
     with Manager() as manager:
         q_measurement_data = manager.Queue()
         q_measurement_frequency = manager.Queue()
+        q_configuration = manager.Queue()
 
         p_sensor = Process(
-            target=sensor_data_process,
+            target=process_sensor_data,
             args=(q_measurement_data, q_measurement_frequency, 5),
         )
-        p_irrigation = Process(target=irrigation_process, args=(q_measurement_data,))
+        p_irrigation = Process(target=process_irrigation, args=(q_measurement_data,))
+        p_configuration = Process(target=process_configuration_manager, args=(q_configuration,))
 
         p_sensor.start()
         p_irrigation.start()
+        p_configuration.start()
 
         p_sensor.join()
         p_irrigation.join()
+        p_configuration.join()
